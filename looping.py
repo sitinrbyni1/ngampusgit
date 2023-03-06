64b409a28aaec3e8ab052fc847dd41ce38285832
sitinrbyni1 <nurbayani0025@gmail.com>
2023-03-06 12:06:07 +0700

    Mengubah sorce code ganjil genap dengan if else

--- looping.py
+++ looping.py
@@ -2,12 +2,7 @@ print('Numbers from 1 to 100:')
 for n in range(1, 101):
     print(n, end=' ')
 
-# untuk mengecek apakah bilangan tersebut genap/ganjil, jika ganjil  maka skip menggunakan if else
 
-for n in range(1, 101):
-    if n % 2 == 0:
-        continue
-    else:
-        print(n)
+#soal ketiga function untuk mengecek sebuah bilangan genap atau ganjil. 
 
-
+def cek_ganjil_genap(angka):
