6169b986c21b4ad1af9b7c7ffaa47d02e61ffc62
sitinrbyni1 <nurbayani0025@gmail.com>
2023-03-06 11:49:50 +0700

    Mengubah source code ganjil genap dengan if else

--- looping.py
+++ looping.py
@@ -1,3 +1,13 @@
 print('Numbers from 1 to 100:')
 for n in range(1, 101):
     print(n, end=' ')
+
+# untuk mengecek apakah bilangan tersebut genap/ganjil, jika ganjil  maka skip menggunakan if else
+
+for n in range(1, 101):
+    if n % 2 == 0:
+        continue
+    else:
+        print(n)
+
+
