8fdf08e3565e0bcb54e1b3a308b49ec6e1426c3e
sitinrbyni1 <nurbayani0025@gmail.com>
2023-03-06 20:56:13 +0700

    menambahkan sc if else

--- looping.py
+++ looping.py
@@ -1,16 +1,5 @@
-# print('Numbers from 1 to 100:')
-# for n in range(1, 101):
-#     print(n, end=' ')
-
-
-# #soal ketiga function untuk mengecek sebuah bilangan genap atau ganjil. 
-
-# def cek_ganjil_genap(angka):
-
-    # untuk mengecek apakah bilangan tersebut genap/ganjil, jika ganjil  maka skip menggunakan if else
-
+print('Numbers from 1 to 100:')
 for n in range(1, 101):
-    if n % 2 == 0:
-        continue
-    else:
-        print(n)
+    print(n, end=' ')
+
+  
