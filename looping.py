376f5d65ee67a2942ba54dd89d84f1623f12f122
sitinrbyni1 <nurbayani0025@gmail.com>
2023-03-06 21:32:02 +0700

    update source code

--- looping.py
+++ looping.py
@@ -1,3 +1,4 @@
+#looping 1-100
 print('Numbers from 1 to 100:')
 for n in range(1, 101):
     print(n, end=' ')
--- looping2.py
+++ looping2.py
@@ -1,5 +1,4 @@
-  # untuk mengecek apakah bilangan tersebut genap/ganjil, jika ganjil  maka skip menggunakan if else
-
+# untuk mengecek apakah bilangan tersebut genap/ganjil, jika ganjil  maka skip menggunakan if else
 for n in range(1, 101):
     if n % 2 == 0:
         continue
