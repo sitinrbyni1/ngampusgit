13fdd7f21730259e12eb73d3683cb062ceb7938d
sitinrbyni1 <nurbayani0025@gmail.com>
2023-03-06 20:34:49 +0700

    Menambahlan sc ganjil genap menggunakan if else

--- looping.py
+++ looping.py
@@ -1,8 +1,16 @@
-print('Numbers from 1 to 100:')
+# print('Numbers from 1 to 100:')
+# for n in range(1, 101):
+#     print(n, end=' ')
+
+
+# #soal ketiga function untuk mengecek sebuah bilangan genap atau ganjil. 
+
+# def cek_ganjil_genap(angka):
+
+    # untuk mengecek apakah bilangan tersebut genap/ganjil, jika ganjil  maka skip menggunakan if else
+
 for n in range(1, 101):
-    print(n, end=' ')
-
-
-#soal ketiga function untuk mengecek sebuah bilangan genap atau ganjil. 
-
-def cek_ganjil_genap(angka):
+    if n % 2 == 0:
+        continue
+    else:
+        print(n)
--- looping2.py
+++
@@ -1,7 +0,0 @@
-# untuk mengecek apakah bilangan tersebut genap/ganjil, jika ganjil  maka skip menggunakan if else
-
-for n in range(1, 101):
-    if n % 2 == 0:
-        continue
-    else:
-        print(n)
